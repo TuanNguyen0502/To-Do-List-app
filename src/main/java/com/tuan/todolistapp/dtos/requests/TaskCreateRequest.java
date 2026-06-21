a379a5260153be732a1c05131240e3ca50c5b214
Tuan <nguyenhahongtuan@gmail.com>
2026-06-22 01:51:01 +0700

    refactor: remove sorting parameter from getAllTasks method and update related implementations

--- src/main/java/com/tuan/todolistapp/dtos/requests/TaskCreateRequest.java
+++ src/main/java/com/tuan/todolistapp/dtos/requests/TaskCreateRequest.java
@@ -1,18 +1,18 @@
 package com.tuan.todolistapp.dtos.requests;
 
 import com.tuan.todolistapp.commons.constants.MessageConstant;
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class TaskCreateRequest {
     @NotBlank(message = MessageConstant.TASK_TITLE_NOT_BLANK)
-    @Max(value = 255, message = MessageConstant.TASK_TITLE_MAX)
+    @Length(max = 255, message = MessageConstant.TASK_TITLE_MAX)
     private String title;
 
     @NotBlank(message = MessageConstant.TASK_DESCRIPTION_NOT_BLANK)
--- src/main/java/com/tuan/todolistapp/dtos/requests/TaskUpdateRequest.java
+++ src/main/java/com/tuan/todolistapp/dtos/requests/TaskUpdateRequest.java
@@ -2,19 +2,19 @@ package com.tuan.todolistapp.dtos.requests;
 
 import com.tuan.todolistapp.commons.constants.MessageConstant;
 import com.tuan.todolistapp.models.enums.ETaskStatus;
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class TaskUpdateRequest {
     @NotBlank(message = MessageConstant.TASK_TITLE_NOT_BLANK)
-    @Max(value = 255, message = MessageConstant.TASK_TITLE_MAX)
+    @Length(max = 255, message = MessageConstant.TASK_TITLE_MAX)
     private String title;
 
     @NotBlank(message = MessageConstant.TASK_DESCRIPTION_NOT_BLANK)
