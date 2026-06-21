0b6932b71f9ba15815b2dec5ccb42da8b2f7b26b
Tuan <nguyenhahongtuan@gmail.com>
2026-06-22 01:53:02 +0700

    feat: enable CORS for TaskController to allow requests from localhost

--- src/main/java/com/tuan/todolistapp/controllers/TaskController.java
+++ src/main/java/com/tuan/todolistapp/controllers/TaskController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/tasks")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class TaskController {
     private final ITaskService taskService;
 
